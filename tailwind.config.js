2d672923af339cffc96c5d5cb1b80e4b9768d06b
Wawwouth <knyxe.hequinoxteam@gmail.com>
2021-12-25 21:18:32 +0100

    add twitch palette to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@ module.exports = {
   mode: 'jit',
   content: ["./src/**/*.{html,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "twitch-gray" : "#0E0E10",
+        "twitch-gray2": "#18181B",
+        "twitch-gray3": "#1F1F23",
+        "twitch-gray4": "#3E3E40",
+        "twitch-input-gray": "#3E3E40",
+        "twitch-tag-gray": "#323234",
+        "twitch-purple" : "#772CE8",
+        "twitch-purple2" : "#BF94FF",
+        "twitch-purple3" : "#9256ED",
+      },
+      fontFamily: {
+        "rubik-sans": ['Rubik', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
