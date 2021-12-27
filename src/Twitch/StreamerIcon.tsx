c75610c27eed9eab5006e38c63e046f1dd107c6a
Wawwouth <knyxe.hequinoxteam@gmail.com>
2021-12-27 15:02:02 +0100

    add icon as props

--- src/Twitch/StreamerIcon.tsx
+++ src/Twitch/StreamerIcon.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 
 type StreamerIconProps = {
+  icon?: string;
   className?: string
 }
-function StreamerIcon({className}: StreamerIconProps) {
+
+function StreamerIcon({icon, className}: StreamerIconProps) {
   return (
     <button>
       <div 
-        className={`w-7 aspect-square bg-twitch-tag-gray rounded-full ${className ?? ''}`}
+        className={`w-7 aspect-square bg-twitch-tag-gray rounded-full bg-contain ${className ?? ''}`}
+        style={{backgroundImage: icon ? `url('${icon}')` : ''}}
       >
       </div>
     </button>
