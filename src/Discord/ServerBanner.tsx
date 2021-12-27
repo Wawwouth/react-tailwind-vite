4bdfcdf0fe5958640cb5350e06be84589775de14
Wawwouth <knyxe.hequinoxteam@gmail.com>
2021-12-27 17:37:02 +0100

    handle server banners

--- src/Discord/ServerBanner.tsx
+++ src/Discord/ServerBanner.tsx
@@ -1,12 +1,21 @@
+import MaterialIcon from "shared/MaterialIcon"
+import IconButton from "Twitch/IconButton"
 import { ServerData } from "./dummy_data"
 
 export type ServerBannerProps = {
   server: ServerData;
 }
-function ServerBanner({server}: ServerBannerProps) {
+function ServerBanner({ server }: ServerBannerProps) {
   return (
-    <div>
-      banner
+    <div 
+      className={`flex py-2 justify-between px-4 
+        ${server.banner ? 'items-start aspect-video bg-contain bg-no-repeat' : 'items-center'} 
+        border-b border-discord-gray-6
+        `}
+      style={{backgroundImage: server.banner ? `url('${server.banner}')` : ''}}
+    >
+      <span className="font-semibold">{server.name}</span>
+      <IconButton name='expand_more' iconClass="text-discord-gray-2" noHover />
     </div>
   )
 }
--- src/Discord/dummy_data.ts
+++ src/Discord/dummy_data.ts
@@ -1,8 +1,10 @@
 import gamepad from 'resources/img/gamepad-icon.png'
+import banner from 'resources/img/discord_banner.jpg'
 
 export type ServerData = {
   name: string;
   icon?: string;
+  banner?: string;
 }
 
 export type DiscrodDataType = {
@@ -11,18 +13,18 @@ export type DiscrodDataType = {
 
 export const data: DiscrodDataType = {
   servers: [
-    {name: "Serv 1"},
-    {name: "Serv 2", icon: gamepad},
+    {name: "Serv 1", banner},
+    {name: "Serv 2", icon: gamepad, banner},
     {name: "Serv 3"},
-    {name: "Serv 4"},
-    {name: "Serv 5"},
-    {name: "Serv 6"},
-    {name: "Serv 7"},
-    {name: "Serv 8"},
-    {name: "Serv 8"},
-    {name: "Serv 10"},
-    {name: "Serv 11"},
-    {name: "Serv 12"},
-    {name: "Serv 13"},
+    {name: "Serv 4", banner},
+    {name: "Serv 5", banner},
+    {name: "Serv 6", banner},
+    {name: "Serv 7", banner},
+    {name: "Serv 8", banner},
+    {name: "Serv 8", banner},
+    {name: "Serv 10", banner},
+    {name: "Serv 11", banner},
+    {name: "Serv 12", banner},
+    {name: "Serv 13", banner},
   ]
 }
