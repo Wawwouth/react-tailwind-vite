25460584930e08da9621814a65288204aacfdb91
Wawwouth <knyxe.hequinoxteam@gmail.com>
2021-12-27 22:04:10 +0100

    fix title position when no banner

--- src/Discord/ServerBanner.tsx
+++ src/Discord/ServerBanner.tsx
@@ -8,14 +8,15 @@ export type ServerBannerProps = {
 function ServerBanner({ server }: ServerBannerProps) {
   return (
     <div 
-      className={`flex py-2 justify-between px-4 
-        ${server.banner ? 'items-start aspect-video bg-contain bg-no-repeat' : 'items-center'} 
+      className={`${server.banner ? 'items-start aspect-video bg-contain bg-no-repeat' : 'items-center'} 
         border-b border-discord-gray-6
         `}
       style={{backgroundImage: server.banner ? `url('${server.banner}')` : ''}}
     >
-      <span className="font-semibold">{server.name}</span>
-      <IconButton name='expand_more' iconClass="text-discord-gray-2" noHover />
+      <div className="flex items-center h-12 justify-between basis-full py-2 px-4">
+        <span className="font-semibold">{server.name}</span>
+        <IconButton name='expand_more' iconClass="h-12 text-discord-gray-2" noHover />
+      </div>
     </div>
   )
 }
