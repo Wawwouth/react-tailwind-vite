bbc6b04b801d17da9dacc1ffee6cc7a6830d3776
Wawwouth <knyxe.hequinoxteam@gmail.com>
2021-12-27 21:57:43 +0100

    renaming component

--- src/Discord/UsersBar.tsx
+++
@@ -1,9 +0,0 @@
-function UsersBar() {
-  return (
-    <div className="bg-discord-gray-5">
-      USERS
-    </div>
-  )
-}
-
-export default UsersBar
---
+++ src/Discord/UsersList.tsx
@@ -0,0 +1,16 @@
+function UsersList() {
+  return (
+    <div className="w-60 shrink-0
+    flex flex-col
+    bg-discord-gray-5
+      "
+    >
+      {/* USERS LIST */}
+      <div>
+        userslist
+      </div>
+    </div>
+  )
+}
+
+export default UsersList
