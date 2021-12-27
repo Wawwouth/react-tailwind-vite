eae2a768299c635418b325d6107b79ac2e4935eb
Wawwouth <knyxe.hequinoxteam@gmail.com>
2021-12-27 21:58:01 +0100

    dynamic channel selection

--- src/Discord/Discord.tsx
+++ src/Discord/Discord.tsx
@@ -2,15 +2,22 @@ import { useCallback, useState } from "react"
 import ChannelContent from "./ChannelContent"
 import ChannelsBar from "./ChannelsBar"
 import ServerBar from "./ServerBar"
-import UsersBar from "./UsersBar"
+import UsersList from "./UsersList"
 
 import { data } from "./dummy_data"
 
 function Discord() {
   const [currentServer, setCurrentServer] = useState<number>(2)
+  const [currentChannel, setCurrentChannel] = useState<number>(0)
+  
   const serverSelected = useCallback((index: number) => {
     setCurrentServer(index)
   }, [])
+  
+  const channelSelected = useCallback((index: number) => {
+    setCurrentChannel(index)
+  }, [])
+
   return (
     <div className='
       h-full w-full bg-discord-gray-6 text-white overflow-hidden
@@ -19,8 +26,7 @@ function Discord() {
       <ServerBar current={currentServer} onServerSelect={serverSelected} />
       <main className="grow flex">
         <ChannelsBar server={data.servers[currentServer]} />
-        <ChannelContent />
-        <UsersBar />
+        <ChannelContent channel={data.servers[currentServer].channels[currentChannel]} />
       </main>
     </div>
   )
