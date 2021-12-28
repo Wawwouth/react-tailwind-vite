be0d4ed84abef395c20501b31fd3b6a37dc9c85c
Wawwouth <knyxe.hequinoxteam@gmail.com>
2021-12-28 15:01:25 +0100

    override thin scrollbar

--- src/Discord/ServerBar.tsx
+++ src/Discord/ServerBar.tsx
@@ -10,7 +10,7 @@ export type ServerBarProps = {
 
 function ServerBar({current, onServerSelect}: ServerBarProps) {
   return (
-    <nav className="flex flex-col gap-y-2 w-16 p-1 px-2.5 overflow-y-auto [scrollbar-width:none]">
+    <nav className="flex flex-col gap-y-2 w-16 p-1 px-2.5 overflow-y-auto ![scrollbar-width:none]">
       <HomeIcon />
       <hr className="border-t border-discord-gray-3 w-2/3 mx-auto" />
       {data.servers.map((server, i) => (
