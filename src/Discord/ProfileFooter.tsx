e60babc3da1e714f03558c6a1e8c87539fc77f34
Wawwouth <knyxe.hequinoxteam@gmail.com>
2021-12-28 13:05:53 +0100

    fix status x-overflow

--- src/Discord/ProfileFooter.tsx
+++ src/Discord/ProfileFooter.tsx
@@ -3,15 +3,15 @@ import ProfileIcon from './ProfileIcon'
 
 function ProfileFooter() {
   return (
-    <div className={`flex items-center justify-between px-2
+    <div className={`flex items-center justify-between px-2 py-1
     bg-discord-gray-6
     `}>
-      <div className='grow flex items-center gap-x-2'>
-        <ProfileIcon className='hover:bg-opacity-70' />
-        <div className='group flex flex-col leading-tight py-1.5 font-bold text-sm'>
+      <div className='grow min-w-0 flex items-center gap-x-2'>
+        <ProfileIcon className='hover:brightness-90' />
+        <div className='group grow min-w-0 flex flex-col leading-tight py-1.5 font-bold text-sm'>
           <span className='cursor-pointer'>Username</span>
           <div className='text-xs flex flex-col text-discord-gray-2 h-4 overflow-hidden'>
-            <span className='group-hover:-translate-y-4 transition-transform [transition-duration:0.4s]'>status</span>
+            <p className='shrink-0 group-hover:-translate-y-4 transition-transform [transition-duration:0.4s] text-ellipsis whitespace-nowrap overflow-x-hidden'>🐵 custom status 🙊</p>
             <span className='translate-y-4 group-hover:-translate-y-4 transition-transform [transition-duration:0.4s]'>#3670</span>
           </div>
         </div>
