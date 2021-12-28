3cdadfcd6c72e169ad84039f304bf867b65b404a
Wawwouth <knyxe.hequinoxteam@gmail.com>
2021-12-28 20:10:32 +0100

    users display with status and avatar

--- src/Discord/UserListItem.tsx
+++ src/Discord/UserListItem.tsx
@@ -1,13 +1,41 @@
-import { UserData } from "./dummy_data"
+import { ServerRole, UserData } from "./dummy_data"
+import ProfileIcon from "./ProfileIcon"
 
 export type UserListItemProps = {
-  user: UserData
+  user: UserData;
+  role?: ServerRole;
 }
 
-function UserListItem({user}: UserListItemProps) {
+function UserListItem({ user, role }: UserListItemProps) {
   return (
-    <div>
-      {user.username}
+    <div className="flex items-center gap-x-2 p-1 overflow-hidden
+      rounded-md
+      hover:bg-discord-gray-7 cursor-pointer
+    ">
+      <ProfileIcon />
+      <div className="grow flex flex-col overflow-hidden">
+        <span
+          title={user.username}
+          className="
+          text-sm font-semibold
+          text-ellipsis whitespace-nowrap overflow-hidden
+          "
+          style={{ color: role?.color ?? 'theme(colors.discord-gray-2)' }}
+        >
+          {user.username}
+        </span>
+        {user.status && (
+          <span 
+            title={user.status}
+            className="
+            text-xs text-discord-gray-3 font-semibold
+            text-ellipsis whitespace-nowrap overflow-hidden
+            "
+          >
+            {user.status}
+          </span>
+        )}
+      </div>
     </div>
   )
 }
