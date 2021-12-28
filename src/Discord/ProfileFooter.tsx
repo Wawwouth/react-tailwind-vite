49ec42f568e6b87c3b7eb65b97c1bb8e3e74455f
Wawwouth <knyxe.hequinoxteam@gmail.com>
2021-12-28 13:08:53 +0100

    hover the whole footer scrolls status

--- src/Discord/ProfileFooter.tsx
+++ src/Discord/ProfileFooter.tsx
@@ -3,12 +3,12 @@ import ProfileIcon from './ProfileIcon'
 
 function ProfileFooter() {
   return (
-    <div className={`flex items-center justify-between px-2 py-1
+    <div className={`group flex items-center justify-between px-2 py-1
     bg-discord-gray-6
     `}>
       <div className='grow min-w-0 flex items-center gap-x-2'>
         <ProfileIcon className='hover:brightness-90' />
-        <div className='group grow min-w-0 flex flex-col leading-tight py-1.5 font-bold text-sm'>
+        <div className='grow min-w-0 flex flex-col leading-tight py-1.5 font-bold text-sm'>
           <span className='cursor-pointer'>Username</span>
           <div className='text-xs flex flex-col text-discord-gray-2 h-4 overflow-hidden'>
             <p className='shrink-0 group-hover:-translate-y-4 transition-transform [transition-duration:0.4s] text-ellipsis whitespace-nowrap overflow-x-hidden'>🐵 custom status 🙊</p>
